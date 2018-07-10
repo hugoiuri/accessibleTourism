dae14e60343d1c8cb78b51d9a252d2d12a0c9922
Hugo Iuri <hugo@minutrade.com>
2018-07-10 18:07:08 -0300

    adicionando gravatar para foto de perfil

--- app/src/main/java/br/com/hkmc/accessibleTourism/activity/ProfileActivity.java
+++ app/src/main/java/br/com/hkmc/accessibleTourism/activity/ProfileActivity.java
@@ -7,21 +7,24 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.apache.commons.io.IOUtils;
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import br.com.hkmc.accessibleTourism.R;
 import br.com.hkmc.accessibleTourism.services.TokenService;
@@ -32,7 +35,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     TextView txtName;
     TextView txtEmail;
-
+    ImageView imgProfile;
 
 
     public void print(String msg) {
@@ -67,13 +70,18 @@ public class ProfileActivity extends AppCompatActivity {
 
         txtName = findViewById(R.id.txtNameId);
         txtEmail = findViewById(R.id.txtEmailId);
+        imgProfile = findViewById(R.id.imgProfileId);
+
+        UsersService usersService = new UsersService();
+        usersService.execute(username);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.mnProfileId);
+    }
 
-        UsersService usersService = new UsersService();
-        usersService.execute(username);
+    public void btnEditOnClick(View view){
+        print("Funcionalidade disponivel em breve");
     }
 
     public class UsersService extends AsyncTask<String, Void, String> {
@@ -141,13 +149,48 @@ public class ProfileActivity extends AppCompatActivity {
 
                     JSONObject json = new JSONObject(s);
 
-                    txtName.setText(json.getString("name"));
-                    txtEmail.setText(json.getString("email"));
+                    String email = json.getString("email");
+                    String name = json.getString("name");
+
+                    txtName.setText(name);
+                    txtEmail.setText(email);
+
+                    String hash = md5(email);
+
+                    String gravatarUrl = "http://www.gravatar.com/avatar/" + hash + "?s=200&d=200";
+
+                    Picasso.get()
+                            .load(gravatarUrl)
+                            .resize(200, 200)
+                            .centerCrop()
+                            .into(imgProfile);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
+        }
+
+        private final String md5(final String text) {
+            final String MD5 = "MD5";
+            try {
+                MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+                digest.update(text.getBytes());
+                byte messageDigest[] = digest.digest();
+
+                StringBuilder hexString = new StringBuilder();
+                for (byte aMessageDigest : messageDigest) {
+                    String hash = Integer.toHexString(0xFF & aMessageDigest);
+                    while (hash.length() < 2)
+                        hash = "0" + hash;
+                    hexString.append(hash);
+                }
+                return hexString.toString();
+
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            return "";
         }
     }
 }
